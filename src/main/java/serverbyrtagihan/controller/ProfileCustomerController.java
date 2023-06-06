80bdd56c62b90450d05af41d4402118803fb2165
NizarRestu <nizarrestuaji18@gmail.com>
2023-06-06 15:43:36 +0700

    profile customer

--- src/main/java/serverbyrtagihan/controller/ProfileCustomerController.java
+++ src/main/java/serverbyrtagihan/controller/ProfileCustomerController.java
@@ -14,7 +14,7 @@ import serverbyrtagihan.response.ResponseHelper;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/api")
 @CrossOrigin(origins = " http://127.0.0.1:5173")
 public class ProfileCustomerController {
     @Autowired
@@ -22,30 +22,30 @@ public class ProfileCustomerController {
     @Autowired
     private ModelMapper modelMapper;
 
-    @PostMapping(path = "/profile",consumes = "multipart/form-data")
+    @PostMapping(path = "/customer/profile",consumes = "multipart/form-data")
     public CommonResponse<Profile> add(ProfileDTO profileDTO, @RequestPart("file") MultipartFile multipartFile) {
         return ResponseHelper.ok(profileService.add(modelMapper.map(profileDTO, Profile.class), multipartFile));
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/customer/profile/{id}")
     public CommonResponse<Profile> getByID(@PathVariable("id") Long id) {
         return ResponseHelper.ok(profileService.getById(id));
     }
 
-    @GetMapping
+    @GetMapping(path ="/customer/profile")
     public CommonResponse<List<Profile>> getAll() {
         return ResponseHelper.ok(profileService.getAll());
     }
 
-    @PutMapping(path = "/picture/{id}", consumes = "multipart/form-data")
+    @PutMapping(path = "/customer/picture/{id}", consumes = "multipart/form-data")
     public CommonResponse<Profile> putPicture(@PathVariable("id") Long id, PictureDTO profile, @RequestPart("file") MultipartFile multipartFile) {
         return ResponseHelper.ok(profileService.putPicture(modelMapper.map(profile, Profile.class), multipartFile, id));
     }
-    @PutMapping(path = "/{id}")
+    @PutMapping(path = "/customer/profile/{id}")
     public CommonResponse<Profile> put(@PathVariable("id") Long id , ProfileDTO profile){
         return ResponseHelper.ok(profileService.put(modelMapper.map(profile, Profile.class), id));
     }
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/customer/profile/{id}")
     public CommonResponse<?> delete(@PathVariable("id") Long id) {
         return ResponseHelper.ok(profileService.delete(id));
     }
