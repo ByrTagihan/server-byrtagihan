7b9c989d5ab277791fa05e2a3c4782f6012e5373
SandyPutraRamadhan <Sandiputrarama34@gmail.com>
2023-06-26 09:39:10 +0700

    fix import error

--- src/main/java/serverbyrtagihan/repository/BillRepository.java
+++ src/main/java/serverbyrtagihan/repository/BillRepository.java
@@ -1,4 +1,4 @@
-package serverbyrtagihan.repository;
+package serverbyrtagihan.Repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
