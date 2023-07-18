18e7e801bf74e84b09371022f80d0ba3ae2e462a
NizarRestu <nizarrestuaji18@gmail.com>
2023-07-18 21:06:38 +0700

    pagination

--- src/main/java/serverbyrtagihan/controller/PaymentController.java
+++ src/main/java/serverbyrtagihan/controller/PaymentController.java
@@ -2,15 +2,21 @@ package serverbyrtagihan.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import serverbyrtagihan.dto.PaymentDto;
 import serverbyrtagihan.modal.Payment;
+import serverbyrtagihan.modal.Transaction;
 import serverbyrtagihan.response.CommonResponse;
+import serverbyrtagihan.response.PaginationResponse;
 import serverbyrtagihan.response.ResponseHelper;
 import serverbyrtagihan.service.PaymentService;
+import serverbyrtagihan.util.Pagination;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -36,9 +42,33 @@ public class PaymentController {
         return ResponseHelper.ok(paymentService.Preview(id, jwtToken));
     }
     @GetMapping(path = "/user/payment")
-    public CommonResponse<List<Payment>> Get(HttpServletRequest request) {
+    public PaginationResponse<List<Payment>> getAll(
+            HttpServletRequest request,
+            @RequestParam(defaultValue = Pagination.page, required = false) Long page,
+            @RequestParam(defaultValue = Pagination.limit, required = false) Long limit,
+            @RequestParam(defaultValue = Pagination.sort, required = false) String sort,
+            @RequestParam(required = false) String search
+    ) {
         String jwtToken = request.getHeader("Authorization").substring(7);
-        return ResponseHelper.ok(paymentService.Get(jwtToken));
+
+        Page<Payment> paymentPage;
+
+        if (search != null && !search.isEmpty()) {
+            paymentPage = paymentService.getAll(jwtToken, page, limit, sort, search);
+        } else {
+            paymentPage = paymentService.getAll(jwtToken, page, limit, sort, null);
+        }
+
+        List<Payment> payments = paymentPage.getContent();
+        long totalItems = paymentPage.getTotalElements();
+        int totalPages = paymentPage.getTotalPages();
+
+        Map<String, Integer> pagination = new HashMap<>();
+        pagination.put("total", (int) totalItems);
+        pagination.put("page", Math.toIntExact(page));
+        pagination.put("total_page", totalPages);
+
+        return ResponseHelper.okWithPagination(payments, pagination);
     }
     @DeleteMapping(path = "/user/payment/{id}")
     public CommonResponse<?> delete(@PathVariable("id") Long id , HttpServletRequest request) {
--- src/main/java/serverbyrtagihan/repository/PaymentRepository.java
+++ src/main/java/serverbyrtagihan/repository/PaymentRepository.java
@@ -1,9 +1,15 @@
 package serverbyrtagihan.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import serverbyrtagihan.modal.Payment;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query(value = "SELECT * FROM table_payment  WHERE description  LIKE CONCAT('%',:keyword, '%') OR priode LIKE CONCAT('%',:keyword, '%') OR amount LIKE CONCAT('%',:keyword, '%')", nativeQuery = true)
+    Page<Payment> findAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
