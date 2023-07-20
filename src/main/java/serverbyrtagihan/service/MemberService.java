42baa60bb2ac7dc1f24586c28ad1e03d4bda33eb
NizarRestu <nizarrestuaji18@gmail.com>
2023-07-20 08:47:47 +0700

    api member di user

--- src/main/java/serverbyrtagihan/service/MemberService.java
+++ src/main/java/serverbyrtagihan/service/MemberService.java
@@ -10,16 +10,26 @@ import java.util.*;
 public interface MemberService {
     Member add(Member member , String jwtToken);
 
+    Member addInUser(Member member, String jwtToken);
+
     Member getById(Long id , String jwtToken);
+
+    Member getByIdInUser(Long id, String jwtToken);
 
     Member putPass(PasswordDTO member, String jwtToken);
 
 
     Page<Member> getAll(String jwtToken, Long page, Long limit, String sort, String search);
 
+    Page<Member> getAllInUser(String jwtToken, Long page, Long limit, String sort, String search);
+
     Member put(Member member, Long id, String jwtToken);
+
+    Member putInUser(Member member, Long id, String jwtToken);
 
     Member putPassword(Member member, Long id, String jwtToken);
 
     Map<String, Boolean> delete(Long id , String jwtToken);
+
+    Map<String, Boolean> deleteInUser(Long id, String jwtToken);
 }
